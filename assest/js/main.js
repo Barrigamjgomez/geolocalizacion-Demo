bb3188e9a8ea8304ab43fab345da1ea27f57ca86
Mariajose <tienditacote@gmail.com>
2018-02-07 22:33:05 -0300

    btn encuentrame activo

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,70 +1,75 @@
 function initMap(){
 	
-	var LaboratoriaChile = { lat:-33.4488897, lng:-70.6692655 }
+	var laboratoriaChile = { lat:-33.4488897, lng:-70.6692655 }
 	var map = new google.maps.Map(document.getElementById('map'), { 
 		Zoom: 18,
 		center:laboratoriaChile 
   });
 	var marketLaboratoria = new google.maps.Marker({
-		position: LaboratoriaChile,
+		position: laboratoriaChile,
 		map:map
-  });
-}
 
-function buscar(){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(funcionExito,funcionError);
+  });
+
+
+	function buscar(){
+		if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(funcionExito,funcionError);
+		}
 	}
+	document.getElementById("encuentrame").addEventListener("click",buscar);
 
   var latitud, longitud;
   var funcionExito = function (position){
-  	 latitud = posicion.coords.latitude;
-  	longitud = posicion.coords.longitude;
-  } 
+  	 latitud = position.coords.latitude;
+  	longitud = position.coords.longitude;
+  
 
- var miUbicacion = new google.maps.Marker({
-		position:{lat:latitud, lng:longitud}
-		map: map
- 	});
+	 var miUbicacion = new google.maps.Marker({
+			position : {lat:latitud, lng:longitud},
+			nimation: google.maps.Animation.DROP,
+			map: map
+	 	});
 
- map.setZoom(18);
- map.setCenter({lat:latitud, lng:longitud});
+	 map.setZoom(18);
+	 map.setCenter({lat:latitud, lng:longitud});
+  }
+
+	var funcionError = function (error){
+		alert("Tenemos un problema con encontrar tu ubicacion");
+	}
+
+//document.getElementById("encuentrame").addEventListener("click",buscar);
+
+	var inputPartida = document.getElementById("punto-partida");
+	var inputDestino = document.getElementById("punto-destino");
+
+	new google.maps.places.Autocomplete(inputPartida);
+	new google.maps.places.Autocomplete(inputDestino);
+
+	var directionsService = new google.maps.DirectionsService();//obtener coordenadas
+	var directionsDisplay = new google.maps.DirectionsRenderer();//traduce coordenadas de la ruta visible
+
+document.getElementById("traza-ruta").addEventListener("click",trazaRuta);
+	var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+		directionsService,route({
+				origin: inputPartida.value,
+				destination: inputDestino.value,
+				travelMode: "DRIVING"
+			}, function(response, status){
+				if (status === "ok"){
+					directionsDisplay.setDirections(response);
+				}else{
+					window.alert("no encontramos una ruta");
+				};
+		});
+	}
+	directionsDisplay.setMap(map);
+	 var trazaRuta = function(){
+	 	   calculateAndDisplayRoute(directionsService,directionsDisplay);
+	 };
+	 //document.getElementById("traza-ruta").addEventListener("click",trazaRuta);
 }
-
-var functionError = function (error){
- 	alert("Tenemos un problema con encontrar tu ubicacion");
-
-}
-document.getElementById('encuentrame').addEventListener("click",buscar);
-
-var inputPartida = getElementById("punto-partida");
-var inputDestino = getElementById("punto-destino");
-
-new google.maps.places.Autocomplete(inputPartida);
-new google.maps.places.Autocomplete(inputDestino);
-
-var directionsService = new google.maps.DirectionsService();//obtener coordenadas
-var directionsDisplay = new google.maps.DirectionsRenderer();//traduce coordenadas de la ruta visible
-
-var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
-	directionsService,route({
-		origin: inputPartida.value,
-		destination: inputDestino.value,
-		travelMode: "DRIVING"
-	}, function(response, status){
-		if (status === "ok"){
-			directionsDisplay.setDirections(response);
-		}else{
-			window.alert("no encontramos una ruta");
-		}
-	});
-}
-directionsDisplay.setMap(map);
- var trazaRuta = function(){
- 	   calculateAndDisplayRoute(directionsService,directionsDisplay);
- };
- document.getElementById("traza-ruta").addEventListener("click",trazaRuta);
-
 
 /*
 function findMe(){
